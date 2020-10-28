7aa8bf8d44a918b0da5e9c27a1c28a144507cb11
Karst van der Wal <k.a.vanderwal@students.uu.nl>
2020-10-28 16:58:46 +0100

    Clean up controller

--- gloss-counter/src/Controller.hs
+++ gloss-counter/src/Controller.hs
@@ -10,22 +10,15 @@ import System.Random
 
 -- | Handle one iteration of the game
 step :: Float -> World -> IO World
-step secs gstate
-  | elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES
-  = -- We show a new random number
-    do randomNumber <- randomIO
-       let newNumber = abs randomNumber `mod` 10
-       return $ World (ShowANumber newNumber) 0
-  | otherwise
-  = -- Just update the elapsed time
-    return $ gstate { elapsedTime = elapsedTime gstate + secs }
+step secs gstate =
+    return $ gstate
 
 -- | Handle user input
 input :: Event -> World -> IO World
-input e gstate = return (inputKey e gstate)
+input e gstate = return gstate
 
-inputKey :: Event -> World -> World
-inputKey (EventKey (Char c) _ _ _) gstate
-  = -- If the user presses a character key, show that one
-    gstate { infoToShow = ShowAChar c }
-inputKey _ gstate = gstate -- Otherwise keep the same
+--inputKey :: Event -> World -> World
+--inputKey (EventKey (Char c) _ _ _) gstate
+-- = -- If the user presses a character key, show that one
+--    gstate { infoToShow = ShowAChar c }
+--inputKey _ gstate = gstate -- Otherwise keep the same
