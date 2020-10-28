a785f71dc99a0c6c5c99a8b4bb6ed16474e78635
Wim <wimvanderlugt@gmail.com>
2020-10-28 16:46:04 +0100

    Opruimen nutteloze zooi

--- gloss-counter/src/Model.hs
+++ gloss-counter/src/Model.hs
@@ -2,19 +2,9 @@
 --   which represent the state of the game
 module Model where
 
-data InfoToShow = ShowNothing
-                | ShowANumber Int
-                | ShowAChar   Char
-
-nO_SECS_BETWEEN_CYCLES :: Float
-nO_SECS_BETWEEN_CYCLES = 5
-
-data GameState = Play [Rock] Ship UFO [Bullet] | GameOver
-                 
-
-initialState :: GameState
+initialState :: World
 initialState = Play 
-                [Rock (150,150) 20 (2,6)]
+                [Asteroid (150,150) 20 (2,6)]
                 (Ship (0,0)(0,0)) 
                 (UFO (75,75) (2,5)) 
                 []
--- gloss-counter/src/View.hs
+++ gloss-counter/src/View.hs
@@ -5,11 +5,8 @@ module View where
 import Graphics.Gloss
 import Model
 
-view :: GameState -> IO Picture
+view :: World -> IO Picture
 view = return . viewPure
 
-viewPure :: GameState -> Picture
-viewPure gstate = case infoToShow gstate of
-  ShowNothing   -> blank
-  ShowANumber n -> color green (text (show n))
-  ShowAChar   c -> color green (text [c])
+viewPure :: World -> Picture
+viewPure gstate = undefined
