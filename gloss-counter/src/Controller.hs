92ee1404eb17f0bcc20d456d9e5d4f74057a8c36
Karst van der Wal <k.a.vanderwal@students.uu.nl>
2020-10-29 00:46:15 +0100

    Input thrustForward and shoot
    Ik heb beetje input gedaan. Het werkt wel maar A en D nog niet voor het draaien van het schip. Je kan ook schieten maar bullets worden nog niet getekend

--- gloss-counter/src/Controller.hs
+++ gloss-counter/src/Controller.hs
@@ -10,15 +10,40 @@ import System.Random
 
 -- | Handle one iteration of the game
 step :: Float -> World -> IO World
-step secs gstate =
-    return $ gstate
+step secs (Play as (Ship shipCs shipSpeed) ufo bs) =
+    return $ (Play as (Ship updateShipCs updateShipSpeed) ufo bs)
+
+      where updateShipCs :: Coordinates
+            updateShipCs = shipCs .+ (secs .* shipSpeed)
+
+            updateShipSpeed :: Speed
+            updateShipSpeed = shipSpeed .- (secs .* (0,1))
 
 -- | Handle user input
 input :: Event -> World -> IO World
-input e gstate = return gstate
+input (EventKey (Char char) Down _ _) world
+    | char == 'w' = return (thrustForward world)
+    | char == 'a' = return (turnLeft world)
+    | char == 'd' = return (turnRight world)
+    | char == ' ' = return (shoot world)
+    | otherwise = return world
+input _ world = return world
 
 --inputKey :: Event -> World -> World
 --inputKey (EventKey (Char c) _ _ _) gstate
 -- = -- If the user presses a character key, show that one
 --    gstate { infoToShow = ShowAChar c }
---inputKey _ gstate = gstate -- Otherwise keep the same
+--inputKey _ gstate = gstate -- otherwise keep the same
+
+thrustForward :: World -> World
+thrustForward (Play as (Ship shipCs (x,y)) ufo bs) = Play as (Ship shipCs (x,y+10)) ufo bs
+
+turnRight :: World -> World
+turnRight (Play as (Ship cs (x,y)) ufo bs) = undefined
+
+turnLeft :: World -> World
+turnLeft (Play as (Ship cs (x,y)) ufo bs) = undefined
+
+shoot :: World -> World
+shoot (Play as ship@(Ship shipCs shipSpeed) ufo bs) = Play as ship ufo (newBullet:bs)
+    where newBullet = Bullet shipCs (0,30) 0
